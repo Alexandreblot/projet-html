e9aaa473685c3d7ac38ab3bc96ec04221e2329df
Alexandreblot <alexandre.blot@isen-ouest.yncrea.fr>
2025-04-28 11:13:34 +0200

    a

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,6 +1,5 @@
 function main() {
-    console.log("tralala")
-    // displayDateTime();
+    
 }
 
 main();
--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,22 +1,8 @@
-function get2DContext(id) {
-  let canvas = document.getElementById("demo");
-  let context = canvas.getContext("2d");
-  return context;
-}
-
 function displayDateTime() {
-  let tmp = get2DContext("temps");
   const event = new Date();
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  let date = event.toLocaleDateString("fr-FR", options);
   let time = event.toLocaleTimeString("fr-FR");
-  document.getElementById("date").innerText = date;
   document.getElementById("time").innerText = time;
+  setInterval(displayDateTime, 1000);
 }
 
 displayDateTime();

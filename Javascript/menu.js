88da0013ac8b376d138dd53b67ed71735971dfe1
Alexandreblot <alexandre.blot@isen-ouest.yncrea.fr>
2025-05-20 10:20:12 +0200

    f

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,20 +1,24 @@
-let seconde = 0;
+
 function displayDateTime() {
   const event = new Date();
   let time = event.toLocaleTimeString("fr-FR");
-  document.getElementById("time").innerText = " Il est " +time;
+  document.getElementById("time").innerText = " Il est " + time;
   setInterval(displayDateTime, 1000);
 }
 
 function displaycrementSeconds() {
-  seconde = seconde + 1;
-  document.getElementById("phrase2").innerText = "vous etes sur la page depuis ";
+  let seconde = 0;
+  document.getElementById("phrase2").innerText ="vous etes sur la page depuis ";
   document.getElementById("seconde").innerText = seconde + "s";
+  stopfonction = setInterval(function () {
+    document.getElementById("seconde").innerText = seconde + "s";
+    seconde++;
+  }, 1000);
+  seconde++;
 }
 
-
 function main() {
-  setInterval(displaycrementSeconds,1000);
+  displaycrementSeconds();
   displayDateTime();
 }
 main();
